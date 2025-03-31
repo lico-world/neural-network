d9c9a3d32dc47c30650a7895dda985281ffe1c89
lico <kilian.vincent@outlook.com>
2025-03-31 20:14:09 +0200

    Changes script to only launch executable if build process succeed

--- build.ps1
+++ build.ps1
@@ -1,4 +1,12 @@
 Set-Location build
 cmake ..
+
+# Run mingw32-make and check if it succeeds
 mingw32-make
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "Error in the building process." -ForegroundColor Red
+    exit $LASTEXITCODE
+}
+
+# If no error, run the executable
 .\NeuralNetwork
